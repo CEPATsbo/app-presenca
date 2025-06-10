3b147b2280f3f95c3f16bbce5683b3cafccd67d8
Carlos Reis <cepaulodetarso.sbo@gmail.com>
2025-06-09 21:27:15 -0300

    Altera coordenadas para teste de localização

--- public/script.js
+++ public/script.js
@@ -31,8 +31,8 @@ document.addEventListener('DOMContentLoaded', () => {
     ];
 
     // --- CONFIGURAÇÕES DO LOCAL ---
-    const CASA_ESPIRITA_LAT = -22.7532;
-    const CASA_ESPIRITA_LON = -47.3334;
+    const CASA_ESPIRITA_LAT = -22.754655;
+    const CASA_ESPIRITA_LON = -47.402106;
     const RAIO_EM_METROS = 30;
 
     // Elementos da página
