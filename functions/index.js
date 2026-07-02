2d519172036af6fcf5b46a84f85c31e9c4d7e70b
Carlos Reis <cepaulodetarso.sbo@gmail.com>
2026-07-02 14:31:49 -0300

    Criação Samaritanos1

--- functions/index.js
+++ functions/index.js
@@ -11,6 +11,7 @@ const bibliotecaMod = require("./modulos/biblioteca");
 const socialMod = require("./modulos/social");
 const comunicacoesMod = require("./modulos/comunicacoes");
 const instagramMod = require("./modulos/instagram");
+const samaritanosMod = require("./modulos/samaritanos");
 
 // Exportação que o Firebase vai ler (Usando o seu padrão original)
 module.exports = {
@@ -19,5 +20,6 @@ module.exports = {
     ...bibliotecaMod,
     ...socialMod,
     ...comunicacoesMod,
-    ...instagramMod
+    ...instagramMod,
+    ...samaritanosMod
 };
